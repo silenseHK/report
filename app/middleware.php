6d3c4c89ef2bba45f192fba18b265a28366f85d6
Martin <1808402272@qq.com>
2021-10-11 15:10:00 +0800

    新增：默认允许跨域请求

--- app/middleware.php
+++ app/middleware.php
@@ -7,5 +7,9 @@ return [
     // 多语言加载
     // \think\middleware\LoadLangPack::class,
     // Session初始化
-    // \think\middleware\SessionInit::class
+    // \think\middleware\SessionInit::class,
+
+    // 允许跨域请求 (如果没有跨域需求可将此处代码屏蔽以提升安全性)
+    \cores\middleware\AllowCrossDomain::class,
+
 ];
